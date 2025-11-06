f2a67f39305cd61947bbe5c9e2f4a4262e025fe9
Kang Mardhex <74248551+kmardhex@users.noreply.github.com>
2025-11-06 10:33:02 +0700

    Add files via upload

--- go.sh
+++ go.sh
@@ -73,7 +73,7 @@ sleep 0.5
 
 # =====================================================
 # VERIFIKASI FILE
-FILES=("otp" "rfs" "cekplp" "cplp" "dlt" "dor" "loop" "update" "plp.txt" "plp2.txt")
+FILES=("otp" "rfs" "cekplp" "cplp" "dlt" "dor" "loop" "update" "plp.txt" "plp2.txt" "p" "r")
 MISSING=false
 for FILE in "${FILES[@]}"; do
     if [ ! -f "$INSTALL_DIR/$FILE" ]; then
--- motd_static.sh
+++ motd_static.sh
@@ -1,22 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# ==========================================
 #   Termux Neon MOTD by mDx_Dev
-# ==========================================
-RESET="\033[0m"
-BOLD="\033[1m"
-PINK="\033[1;35m"
-CYAN="\033[1;36m"
-YELLOW="\033[1;33m"
-WHITE="\033[0;37m"
+# Warna ANSI
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+BOLD='\033[1m'
+RESET='\033[0m'
 
-clear
-echo -e "${PINK}╔══════════════════════════════════════════════╗${RESET}"
-echo -e "${PINK}║${RESET} ${BOLD}${CYAN}  ___         _                         _   ${RESET}${PINK}║${RESET}"
-echo -e "${PINK}║${RESET} ${BOLD}${CYAN} / _ \\ _   _ | |_  ___  _ __   ___  ___| |_ ${RESET}${PINK}║${RESET}"
-echo -e "${PINK}║${RESET} ${BOLD}${CYAN}| | | | | | || __|/ _ \\| '_ \\ / _ \\/ __| __|${RESET}${PINK}║${RESET}"
-echo -e "${PINK}║${RESET} ${BOLD}${CYAN}| |_| | |_| || |_|  __/| | | |  __/\\__ \\ |_ ${RESET}${PINK}║${RESET}"
-echo -e "${PINK}║${RESET} ${BOLD}${CYAN} \\__\\_\\__,_| \\__|\\___||_| |_|\\___||___/\\__|${RESET}${PINK}║${RESET}"
-echo -e "${PINK}╚══════════════════════════════════════════════╝${RESET}"
+echo -e "${YELLOW}╔══════════════════════════════════════════════╗${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN}  __  __  ____  __   __     ____             ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN} |  \/  |(  _ \\(  ) (  )   (  _ \\            ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN} | \\  / || | | |/ _ \\/ _ \\  | | | | ___  __  ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN} | |\\/| || | | | (_) | (_) ) | | | |/ _ \\(  ) ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN} | |  | || |_/ |\\__, |\\__, | | |_/ | (_) )/\\  ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}║${RESET} ${BOLD}${CYAN} |_|  |_||____/   /_/   /_/  |____/ \\___/(__) ${RESET}${YELLOW}║${RESET}"
+echo -e "${YELLOW}╚══════════════════════════════════════════════╝${RESET}"
 
 echo -e "${CYAN}- Welcome To Cyber Indonesia -${RESET}"
 echo -e "${YELLOW}- Diam Bukan Berarti Kalah, Tapi Sedang Mengamati -${RESET}"
